8ef8e100236b04d3d54890721875797dd57157fc
niueryuf <49442716+niueryuf@users.noreply.github.com>
2020-08-27 11:40:21 +0800

    Update EurekaserverApplication.java

--- src/main/java/com/springcloud/eureka/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/springcloud/eureka/eurekaserver/EurekaserverApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		int i = 89/0;
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 
